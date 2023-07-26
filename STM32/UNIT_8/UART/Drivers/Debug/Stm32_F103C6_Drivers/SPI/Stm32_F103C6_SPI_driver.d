c509d425270216cf52f02ab4a20b1a940b6a297f
ahmeedusamaa <ahmedosama21049@gmail.com>
2023-07-26 16:42:04 +0300

    UART & SPI

---
+++ STM32/UNIT_8/UART/Drivers/Debug/Stm32_F103C6_Drivers/SPI/Stm32_F103C6_SPI_driver.d
@@ -0,0 +1,2 @@
+Stm32_F103C6_Drivers/SPI/Stm32_F103C6_SPI_driver.o: \
+ ../Stm32_F103C6_Drivers/SPI/Stm32_F103C6_SPI_driver.c
---
+++ STM32/UNIT_8/UART/Drivers/Stm32_F103C6_Drivers/SPI/Stm32_F103C6_SPI_driver.c
@@ -0,0 +1,8 @@
+/*
+ * Stm32_F103C6_SPI_driver.c
+ *
+ *  Created on: Jul 24, 2023
+ *      Author: ahmed osama
+ */
+
+
